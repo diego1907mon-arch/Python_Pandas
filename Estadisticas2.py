5eeaf06f6e57847778dd717d7c2798e645fc3469
JUAN DAVID ARENAS GARCIA <david1072191295@gmail.com>
2025-12-05 15:47:19 -0500

    feat,Agregar clase Estadisticas2 con métodos para análisis de infracciones y exceso de velocidad

--- Estadisticas2.py
+++ Estadisticas2.py
@@ -8,4 +8,16 @@ class Estadisticas2:
     def infracciones_por_tipo(self):
         return self.df["Tipo_infracción"].value_counts()
 
-    
+    def infracciones_por_marca(self, top=5):
+        return self.df["Marca_vehículo"].value_counts().head(top)
+
+    def camaras_con_mas_infracciones(self, top=5):
+        return self.df.groupby("ID_cámara")["ID_foto"].count().sort_values(ascending=False).head(top)
+
+    def promedio_por_tipo_via(self):
+        return self.df.groupby("Tipo_vía")["Velocidad_detectada"].mean()
+
+    def porcentaje_infracciones_graves(self):
+        total = len(self.df)
+        graves = self.df[self.df["Tipo_infracción"] == "Grave"].shape[0]
+        return (graves / total) * 100
